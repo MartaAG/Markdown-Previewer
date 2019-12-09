44e8d78a1d4f46ef46e89a6566134841656eacf6
Marta G <marta.guszek@gmail.com>
2019-12-09 14:54:33 +0100

    Add additional tests to check preview

--- src/components/Preview.spec.js
+++ src/components/Preview.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Preview from './Preview'
 
-describe('Editor', () => {
+describe('Preview', () => {
   let wrapper;
   beforeEach(() => wrapper = shallow(<Preview displayed='Enter text'/>));
 
@@ -12,14 +12,19 @@ describe('Editor', () => {
 
   it('should render displayed message in markup editor as HTML', () => {
     //  wrapper.setProps({displayed : 'Enter text'})
-    let rendered_text = "<div id=\"preview\"><p>Enter text</p></div>"
+    let rendered_text = "<div id=\"preview\"><p>Enter text</p></div>";
     expect(wrapper.find("#preview").html()).toEqual(rendered_text)
   })
 
   it('no problem with empty string in editor', () => {
     wrapper.setProps({displayed : ''})
-    let rendered_text = "<div id=\"preview\"></div>"
+    let rendered_text = "<div id=\"preview\"></div>";
     expect(wrapper.find("#preview").html()).toEqual(rendered_text)
+  })
+
+  it('should render displayed message as h1 tag', () => {
+    wrapper.setProps({displayed : '# Enter text'})
+    expect(wrapper.find('#enter-text').length).toEqual(1);
   })
 
 });
