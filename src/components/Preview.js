2efb08a16280363b9bc91fdb78c276e13fc3ec26
Marta G <marta.guszek@gmail.com>
2019-12-09 13:48:44 +0100

    Everything works, but input is not sanitized

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Preview.css'
+import marked from 'marked'
+import ReactHtmlParser from 'react-html-parser'
 
 function Preview(props) {
   return (<div className="container3">
     <p>Preview</p>
     <div id="preview">
-      <p>{props.displayed}</p>
+        {ReactHtmlParser(marked(props.displayed, {breaks:true}))}
     </div>
   </div>)
 }
--- src/components/Preview.spec.js
+++ src/components/Preview.spec.js
@@ -4,10 +4,22 @@ import Preview from './Preview'
 
 describe('Editor', () => {
   let wrapper;
-  beforeEach(() => wrapper = shallow(<Preview />));
+  beforeEach(() => wrapper = shallow(<Preview displayed='Enter text'/>));
 
   it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('div').length).toEqual(2);
   });
+
+  it('should render displayed message in markup editor as HTML', () => {
+    //  wrapper.setProps({displayed : 'Enter text'})
+    let rendered_text = "<div id=\"preview\"><p>Enter text</p></div>"
+    expect(wrapper.find("#preview").html()).toEqual(rendered_text)
+  })
+
+  it('no problem with empty string in editor', () => {
+    wrapper.setProps({displayed : ''})
+    let rendered_text = "<div id=\"preview\"></div>"
+    expect(wrapper.find("#preview").html()).toEqual(rendered_text)
+  })
 
 });
